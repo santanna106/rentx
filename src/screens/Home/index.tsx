8f740b747b23bce422caec4f8033c3844b77f869
Gabriel Andrade <gabriel.santana@gmail.com>
2021-12-28 12:06:28 -0300

    Criação do tema Global e desenvolvimento do Header

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import Logo from '../../assets/logo.svg';
+
+import {
+  Container,
+  Header
+} from './styles';
+
+export function Home(){
+  return (
+    <Container>
+        <StatusBar
+          barStyle='light-content'
+          backgroundColor="transparent"
+          translucent
+         />
+        <Header>
+          <Logo />
+        </Header>
+    </Container>
+  );
+}
