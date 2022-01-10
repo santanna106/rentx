9a8eae197a27f5d1aea86d4ae082492c6a03032c
Gabriel Andrade <gabriel.santana@gmail.com>
2022-01-10 04:13:21 -0300

    Customização dos botões do TabMenu

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { Platform } from 'react-native';
+import { useTheme } from 'styled-components';
+
+import HomeSvg from '../assets/home.svg';
+import PeopleSvg from '../assets/people.svg';
+import CarSvg from '../assets/car.svg';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -7,16 +13,52 @@ import { Home } from '../screens/Home';
 import { MyCars } from '../screens/MyCars';
 import { SignUpSecondStep } from '../screens/SignUp/SignUpSecondStep';
 
+
 const {Navigator,Screen} = createBottomTabNavigator();
 
 export function AppTabRoutes() {
+    const theme = useTheme();
+
     return (
      
-        <Navigator>
-          <Screen name="Home" component={AppStackRoutes} />
-          <Screen name="Profile" component={Home} />
-          <Screen name="SignUpSecondStep" component={SignUpSecondStep} />
-          <Screen name="MyCars" component={MyCars} />
+        <Navigator
+        screenOptions={{
+         
+          tabBarActiveTintColor: theme.colors.main,
+          tabBarInactiveTintColor: theme.colors.text_detail,
+          tabBarShowLabel:false,
+          headerStyle : {
+            paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+            height: 78,
+            backgroundColor:theme.colors.background_primary
+          }
+         
+        }}
+        >
+          <Screen name="Home" 
+                  component={AppStackRoutes} 
+                  options={{
+                    tabBarIcon: (({ color }) => (
+                      <HomeSvg width={24} height={24} fill={color}/>
+                    ))
+                  }}
+          />
+          <Screen name="Profile"
+                  component={Home} 
+                  options={{
+                    tabBarIcon: (({ color }) => (
+                      <CarSvg width={24} height={24} fill={color}/>
+                    ))
+                  }}
+          />
+          <Screen name="MyCars" 
+                  component={MyCars}
+                  options={{
+                    tabBarIcon: (({ color }) => (
+                      <PeopleSvg width={24} height={24} fill={color} />
+                    ))
+                  }}        
+          />
         </Navigator>
     );
   }
