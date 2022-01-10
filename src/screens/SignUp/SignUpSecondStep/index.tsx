d94d8ac278f612dab4021533ec89448ac1046393
Gabriel Andrade <gabriel.santana@gmail.com>
2022-01-10 02:06:50 -0300

    Aplicativo interagindo com backend

--- src/screens/SignUp/SignUpSecondStep/index.tsx
+++ src/screens/SignUp/SignUpSecondStep/index.tsx
@@ -9,7 +9,7 @@ import {
 import { useNavigation,useRoute } from '@react-navigation/native';
 import { useTheme } from 'styled-components';
 
-import { Confirmation } from '../../Confirmation';
+import api from '../../../services/api';
 import { BackButton } from '../../../components/BackButton'
 import { Bullet } from '../../../components/Bullet';
 import { PasswordInput } from '../../../components/PasswordInput';
@@ -24,6 +24,7 @@ import {
   Form,
   FormTitle
 } from './styles';
+
 
 interface Params {
   user:{
@@ -55,13 +56,25 @@ export function SignUpSecondStep(){
       return Alert.alert('As senhas não são iguais');
     }
 
-    navigation.navigate('Confirmation',{
-     
-        nextScreenRoute: 'SignIn',
-        title:'Conta Criada!',
-        message:`Agora é só fazer login\ne aproveitar`
-      
+    await api.post('/users',{
+      name:user.name,
+      email:user.email,
+      driver_license:user.driverLicense,
+      password
+    }).then(() => {
+          navigation.navigate('Confirmation',{
+        
+            nextScreenRoute: 'SignIn',
+            title:'Conta Criada!',
+            message:`Agora é só fazer login\ne aproveitar`
+          
+        })
     })
+    .catch(() => {
+      Alert.alert('Opa','Não foi possível cadastrar');
+    })
+
+   
     
   }
 
